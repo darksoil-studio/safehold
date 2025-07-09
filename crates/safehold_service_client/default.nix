0e8d77905b35484a03c6411892f9838fce704618
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-09 13:48:54 +0200

    Attempt

--- crates/safehold_service_client/default.nix
+++ crates/safehold_service_client/default.nix
@@ -21,7 +21,7 @@
         buildInputs =
           inputs.holochain-nix-builders.outputs.dependencies.${system}.holochain.buildInputs;
         LIBCLANG_PATH = "${pkgs.llvmPackages_18.libclang.lib}/lib";
-        cargoExtraArgs = " -j 4 ";
+        cargoExtraArgs = " -j 3 ";
       };
       cargoArtifacts = craneLib.buildDepsOnly commonArgs;
       binary =
