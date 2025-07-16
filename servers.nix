215c2364a0d3ad0051bfb1a358ba2a05c63d3efd
guillem.cordoba <guillem.cordoba@gmail.com>
2025-07-16 15:13:35 +0200

    Fix

--- servers.nix
+++ servers.nix
@@ -10,52 +10,40 @@ let
     services.openssh.settings.PermitRootLogin = "without-password";
   };
 
-  safehold_service_provider =
+  safehold-service-provider =
     inputs.self.outputs.packages."x86_64-linux".safehold-service-provider;
 
-  safehold_service_provider_module = {
-    systemd.services.safehold_service_provider1 = {
+  safehold-service-provider-module = {
+    systemd.services.safehold-service-provider = {
       enable = true;
-      path = [ safehold_service_provider ];
+      path = [ safehold-service-provider ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart =
-          "${safehold_service_provider}/bin/safehold-service-provider --data-dir /root/safehold-service-provider1";
+          "${safehold-service-provider}/bin/safehold-service-provider --data-dir /root/safehold-service-provider";
         RuntimeMaxSec = "3600"; # Restart every hour
 
         Restart = "always";
         RestartSec = 1;
       };
     };
-    systemd.services.safehold_service_provider2 = {
-      enable = true;
-      path = [ safehold_service_provider ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        ExecStart =
-          "${safehold_service_provider}/bin/safehold-service-provider --data-dir /root/safehold-service-provider2";
-        RuntimeMaxSec = "3600"; # Restart every hour
-
-        Restart = "always";
-        RestartSec = 1;
-      };
-    };
-    system.stateVersion = "25.05";
-    garnix.server.enable = true;
-    garnix.server.persistence.enable = true;
   };
 
 in {
   flake = {
-
     nixosConfigurations = {
       safehold-service-provider1 = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           inputs.garnix-lib.nixosModules.garnix
           sshModule
-          { garnix.server.persistence.name = "safehold-service-provider1"; }
-          safehold_service_provider_module
+          safehold-service-provider-module
+          {
+            garnix.server.persistence.name = "safehold-service-provider1";
+            system.stateVersion = "25.05";
+            garnix.server.enable = true;
+            garnix.server.persistence.enable = true;
+          }
         ];
       };
       safehold-service-provider2 = inputs.nixpkgs.lib.nixosSystem {
@@ -63,8 +51,13 @@ in {
         modules = [
           inputs.garnix-lib.nixosModules.garnix
           sshModule
-          { garnix.server.persistence.name = "safehold-service-provider2"; }
-          safehold_service_provider_module
+          safehold-service-provider-module
+          {
+            garnix.server.persistence.name = "safehold-service-provider2";
+            system.stateVersion = "25.05";
+            garnix.server.enable = true;
+            garnix.server.persistence.enable = true;
+          }
         ];
       };
     };
